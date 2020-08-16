bca07fbed23f101df697e9bb09ce3f4ccffbb070
Sarthak Gupta <sarthakgupta072@gmail.com>
2020-08-16 14:22:58 +0530

    init repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# DRS-cient
+
---
+++ drs_client/test.py
@@ -0,0 +1,10 @@
+
+
+
+# File only for testing. Will be removed in the future commits
+
+from drs_client.client import DRSClient
+
+
+client = DRSClient()
+print(client.get_object("t0ltnK"))
